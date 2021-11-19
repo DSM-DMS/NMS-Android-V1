bc891aebc24e13da1ad4346c46c73bbe3d99ec96
aahspringaa4 <aahspringaa4@naver.com>
2021-11-19 17:17:22 +0900

    init

---
+++ app/src/main/java/com/example/nms_android_v1/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.nms_android_v1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
