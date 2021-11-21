a8d32f447f95411f1dc54bbf6352177b4cfb7122
limsaehyun <sh00710000@gmail.com>
2021-11-21 20:10:06 +0900

    Feat :: 게시글 엑티비티 생성

---
+++ app/src/main/java/com/example/nms_android_v1/ui/PostActivity.kt
@@ -0,0 +1,11 @@
+package com.example.nms_android_v1.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post)
+    }
+}
