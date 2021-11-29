d3c3970f22c31ecce7fe0b4148b1fd3a17f61933
saehyun <sh00710000@gmail.com>
2021-11-29 13:29:16 +0900

    Feat :: LoginModule

--- app/src/main/java/com/example/nms_android_v1/di/module/LoginModule.kt
+++ app/src/main/java/com/example/nms_android_v1/di/module/LoginModule.kt
@@ -1,7 +1,11 @@
 package com.example.nms_android_v1.di.module
 
 import com.example.nms_android_v1.data.login.LoginRepository
+import com.example.nms_android_v1.feature.login.viewmodel.LoginViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
 
 val loginModule = module {
-    factory (LoginRepository())
+    factory { LoginRepository() }
+    viewModel { LoginViewModel(get()) }
 }
