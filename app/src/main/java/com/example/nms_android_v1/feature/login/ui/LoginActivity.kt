efbca35a61d85f6a9b1e0ef57367904675b59c96
saehyun <sh00710000@gmail.com>
2021-11-29 13:29:29 +0900

    Feat :: LoginActivity

--- app/src/main/java/com/example/nms_android_v1/feature/login/ui/LoginActivity.kt
+++ app/src/main/java/com/example/nms_android_v1/feature/login/ui/LoginActivity.kt
@@ -3,10 +3,26 @@ package com.example.nms_android_v1.feature.login.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.nms_android_v1.R
+import com.example.nms_android_v1.base.BaseActivity
+import com.example.nms_android_v1.databinding.ActivityLoginBinding
+import com.example.nms_android_v1.feature.login.viewmodel.LoginViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : BaseActivity<ActivityLoginBinding>(
+    R.layout.activity_login
+) {
+
+    val vm: LoginViewModel by viewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+
+    }
+
+    override fun observeEvent() {
+        vm.run {
+
+        }
     }
 }
