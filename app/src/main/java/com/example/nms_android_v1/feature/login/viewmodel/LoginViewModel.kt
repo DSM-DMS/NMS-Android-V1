6d34a54c381721c5c5076798f75866dd4a15cc3d
saehyun <sh00710000@gmail.com>
2021-11-29 13:29:34 +0900

    Feat :: LoginViewModel

--- app/src/main/java/com/example/nms_android_v1/feature/login/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/nms_android_v1/feature/login/viewmodel/LoginViewModel.kt
@@ -1,4 +1,28 @@
 package com.example.nms_android_v1.feature.login.viewmodel
 
-class LoginViewModel {
+import accessToken
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.nms_android_v1.data.login.LoginRepository
+import com.example.nms_android_v1.feature.login.model.LoginRequest
+import org.koin.core.Koin
+import refreshToken
+
+class LoginViewModel(
+    private val rp: LoginRepository
+) : ViewModel() {
+
+    val success : MutableLiveData<Boolean> = MutableLiveData()
+
+    fun login(loginRequest: LoginRequest) {
+        rp.login(loginRequest).subscribe { response ->
+            if(response.isSuccessful) {
+                accessToken = response.body()?.accessToken.toString()
+                refreshToken = response.body()?.accessToken.toString()
+                success.value = true
+            } else {
+                success.value = true
+            }
+        }
+    }
 }
