e026626b32bf71312323957575ae00ad3bbe42d9
saehyun <sh00710000@gmail.com>
2021-11-29 12:39:33 +0900

    Feat :: BaseFragment

---
+++ app/src/main/java/com/example/nms_android_v1/base/BaseFragment.kt
@@ -0,0 +1,41 @@
+package com.example.nms_android_v1.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import com.saehyun.mcss.R
+
+abstract class BaseFragment<B : ViewDataBinding>(
+    @LayoutRes private val layoutRes: Int
+) : Fragment() {
+    protected lateinit var binding: B
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, layoutRes, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.lifecycleOwner = this
+
+        observeEvent()
+    }
+
+    abstract fun observeEvent()
+
+    fun showToast(msg: String) {
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+    }
+}
