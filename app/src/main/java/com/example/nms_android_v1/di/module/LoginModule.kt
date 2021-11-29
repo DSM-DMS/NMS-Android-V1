61d94dbd123d6651a0fdb72e9bb9751c2855b6af
saehyun <sh00710000@gmail.com>
2021-11-29 13:03:07 +0900

    Feat :: LoginModule

--- app/src/main/java/com/example/nms_android_v1/di/module/LoginModule.kt
+++ app/src/main/java/com/example/nms_android_v1/di/module/LoginModule.kt
@@ -1,4 +1,7 @@
 package com.example.nms_android_v1.di.module
 
-class LoginModule {
+import com.example.nms_android_v1.data.login.LoginRepository
+
+val loginModule = module {
+    factory (LoginRepository())
 }
