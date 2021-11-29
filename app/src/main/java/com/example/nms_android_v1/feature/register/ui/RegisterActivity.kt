055ee5b2b62bb3b67acc610b04c86fcff75738dd
saehyun <sh00710000@gmail.com>
2021-11-29 14:58:43 +0900

    Feat :: Register 의존성 주입

--- app/src/main/java/com/example/nms_android_v1/feature/register/ui/RegisterActivity.kt
+++ app/src/main/java/com/example/nms_android_v1/feature/register/ui/RegisterActivity.kt
@@ -1,12 +1,22 @@
-package com.example.nms_android_v1.feature.register
+package com.example.nms_android_v1.feature.register.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.nms_android_v1.R
+import com.example.nms_android_v1.base.BaseActivity
+import com.example.nms_android_v1.databinding.ActivityRegisterBinding
+import com.example.nms_android_v1.feature.register.viewmodel.RegisterViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class RegisterActivity : AppCompatActivity() {
+class RegisterActivity : BaseActivity<ActivityRegisterBinding>(
+    R.layout.activity_register
+) {
+
+    val vm: RegisterViewModel by viewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+
     }
+
+    override fun observeEvent() {}
 }
