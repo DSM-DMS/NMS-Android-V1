c4765d33301461c7ffea26077c4e9830887b3855
saehyun <sh00710000@gmail.com>
2021-11-30 19:31:13 +0900

    Feat :: EditText TextWatcher

--- app/src/main/java/com/example/nms_android_v1/feature/register/ui/RegisterActivity.kt
+++ app/src/main/java/com/example/nms_android_v1/feature/register/ui/RegisterActivity.kt
@@ -1,6 +1,11 @@
 package com.example.nms_android_v1.feature.register.ui
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import androidx.core.content.ContextCompat
 import com.example.nms_android_v1.R
 import com.example.nms_android_v1.base.BaseActivity
 import com.example.nms_android_v1.databinding.ActivityRegisterBinding
@@ -16,6 +21,18 @@ class RegisterActivity : BaseActivity<ActivityRegisterBinding>(
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        binding.editText.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                binding.btnNext.background = ContextCompat.getDrawable(applicationContext, R.drawable.bg_register_btn_border_on)
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+        })
     }
 
     override fun observeEvent() {}
