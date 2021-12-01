c1fe59b728c43a14c5d908c83bd7e53724cae206
saehyun <sh00710000@gmail.com>
2021-12-01 13:28:31 +0900

    Feat :: 회원가입 곤처리

--- app/src/main/java/com/example/nms_android_v1/feature/register/ui/RegisterActivity.kt
+++ app/src/main/java/com/example/nms_android_v1/feature/register/ui/RegisterActivity.kt
@@ -24,12 +24,20 @@ class RegisterActivity : BaseActivity<ActivityRegisterBinding>(
 
     var nickname: String ?= null
     var name: String ?= null
+    var number: String ?= null
+    var grade: String ?= null
+    var password: String ?= null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        nickName()
+    }
+
+    private fun setTextWatcher() {
         binding.etRgFirst.addTextChangedListener(object: TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                binding.btnNext.background = ContextCompat.getDrawable(applicationContext, R.drawable.bg_register_btn_border_off)
             }
 
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
@@ -40,25 +48,11 @@ class RegisterActivity : BaseActivity<ActivityRegisterBinding>(
             }
 
         })
-
-        val itemList = listOf("1학년", "2학년", "3학년")
-        val adapter = ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, itemList)
-        binding.spinnerGrade.adapter = adapter
-
-        binding.spinnerGrade.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
-            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-                binding.tvSpinnerGrade.setTextColor(ContextCompat.getColor(applicationContext, R.color.black))
-                binding.tvSpinnerGrade.text = itemList[p2]
-            }
-
-            override fun onNothingSelected(p0: AdapterView<*>?) {
-            }
-        }
-
-        nickName()
     }
 
     private fun nickName() {
+        setTextWatcher()
+
         binding.etRgFirst.visibility = View.VISIBLE
         binding.etRgFirst.hint = "닉네임"
 
@@ -72,24 +66,88 @@ class RegisterActivity : BaseActivity<ActivityRegisterBinding>(
     }
 
     private fun name() {
-        binding.etRgFirst.visibility = View.VISIBLE
+        setTextWatcher()
+
         binding.etRgFirst.text = null
         binding.etRgFirst.hint = "이름"
 
         binding.btnNext.setOnClickListener {
             name = binding.etRgFirst.text.toString()
 
-            if(nickname.isNullOrEmpty()) showToast("이름을 입력해주세요.")
+            if(name.isNullOrEmpty()) showToast("이름을 입력해주세요.")
             else numberGrade()
         }
     }
 
     private fun numberGrade() {
+        setTextWatcher()
+
+        binding.etRgFirst.text = null
+        binding.etRgFirst.hint = "학번 (ex.1117)"
+
+        val itemList = listOf("1학년", "2학년", "3학년")
+        val adapter = ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, itemList)
+        binding.spinnerGrade.adapter = adapter
+
+        binding.spinnerGrade.visibility = View.VISIBLE
+        binding.tvSpinnerGrade.visibility = View.VISIBLE
+        binding.spinnerIg.visibility = View.VISIBLE
+
+        binding.spinnerGrade.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                binding.tvSpinnerGrade.setTextColor(ContextCompat.getColor(applicationContext, R.color.black))
+                binding.tvSpinnerGrade.text = itemList[p2]
+
+                grade = p2.toString()
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+            }
+        }
+
+        binding.btnNext.setOnClickListener {
+            number = binding.etRgFirst.text.toString()
+
+            if(number.isNullOrEmpty()) {
+                showToast("학번을 입력해주세요.")
+                return@setOnClickListener
+            }
+            if(grade.isNullOrEmpty()) {
+                showToast("학년을 선택해주세요.")
+                return@setOnClickListener
+            }
+
+            password()
+        }
+
     }
 
     private fun password() {
+        setTextWatcher()
+
+        binding.spinnerGrade.visibility = View.GONE
+        binding.tvSpinnerGrade.visibility = View.GONE
+        binding.spinnerIg.visibility = View.GONE
+
+        binding.etRgFirst.text = null
+        binding.etRgFirst.hint = "비밀번호"
+
+        binding.etRgSecond.visibility = View.VISIBLE
+        binding.etRgSecond.text = null
+
+        binding.btnNext.setOnClickListener {
+            password = binding.etRgFirst.text.toString()
+            if(password == binding.etRgSecond.text.toString()) {
+                success()
+            }
+        }
 
     }
+
+    private fun success() {
+        showToast("완료")
+    }
+
 
     override fun observeEvent() {}
 }
