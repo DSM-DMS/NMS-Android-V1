32f4b2ba0eee9a862139f977cec5f0e30b7361ed
limsaehyun <sh00710000@gmail.com>
2021-12-02 20:13:10 +0900

    Feat :: MainApi

---
+++ app/src/main/java/com/example/nms_android_v1/data/main/MainApi.kt
@@ -0,0 +1,15 @@
+package com.example.nms_android_v1.data.main
+
+import com.example.nms_android_v1.feature.main.model.PostsResponse
+import io.reactivex.rxjava3.core.Single
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Header
+
+interface MainApi {
+    @GET("notice")
+    fun getPosts(
+        @Header("x-refresh-token") refreshToken: String
+    ) : Single<Response<PostsResponse>>
+}
