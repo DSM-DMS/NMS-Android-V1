de60925bec752f8a9d84fbb204de7a6216e9906d
limsaehyun <sh00710000@gmail.com>
2021-12-02 20:13:43 +0900

    Feat :: main 의존성 주입

--- app/src/main/java/com/example/nms_android_v1/feature/main/MainActivity.kt
+++ app/src/main/java/com/example/nms_android_v1/feature/main/MainActivity.kt
@@ -3,10 +3,23 @@ package com.example.nms_android_v1.feature.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.nms_android_v1.R
+import com.example.nms_android_v1.base.BaseActivity
+import com.example.nms_android_v1.databinding.ActivityMainBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(
+    R.layout.activity_main
+) {
+
+    val vm: MainViewModel by viewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    }
+
+    override fun observeEvent() {
+        vm.run {
+
+        }
     }
 }
