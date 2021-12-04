e291d8c23e026aae319b5a26f050cb61cb503ad9
유현명 <aahspringaa4@gmail.com>
2021-12-04 16:59:59 +0900

    Feat :: Server Response Value DTO

---
+++ app/src/main/java/com/example/nms_android_v1/data/ResponseLikePostDTO.kt
@@ -0,0 +1,10 @@
+package com.example.nms_android_v1.data
+
+data class ResponseLikePostDTO(
+    val id : String,
+    val title : String,
+    val writer :  String,
+    val department : String,
+    val created_date : String,
+    val image : String
+)
