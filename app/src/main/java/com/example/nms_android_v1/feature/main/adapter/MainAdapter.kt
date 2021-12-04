46c44d130b7ef65f45f2f58ac373e910c29878bb
limsaehyun <sh00710000@gmail.com>
2021-12-04 17:06:43 +0900

    Feat :: MainAdapter

---
+++ app/src/main/java/com/example/nms_android_v1/feature/main/adapter/MainAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.nms_android_v1.feature.main.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.nms_android_v1.R
+import com.example.nms_android_v1.databinding.MainItemBinding
+import com.example.nms_android_v1.feature.main.model.Notices
+import com.example.nms_android_v1.feature.main.model.Writer
+
+class MainAdapter(
+    val context: Context,
+    val productData: List<Notices>
+
+    ) : RecyclerView.Adapter<MainAdapter.Holder>() {
+
+    private lateinit var binding: MainItemBinding
+
+    inner class Holder(itemView: View?) : RecyclerView.ViewHolder(itemView!!) {
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        binding = DataBindingUtil.inflate<MainItemBinding>(LayoutInflater.from(parent.context),
+        R.layout.main_item, parent, false)
+
+
+        return Holder(binding.root)
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        var data: Notices = productData.get(position)
+    }
+
+    override fun getItemCount(): Int {
+        return productData.size
+    }
+}
