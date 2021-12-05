9def1d713f57fe671d91596ff1cc820d16bf648d
limsaehyun <sh00710000@gmail.com>
2021-12-05 21:04:25 +0900

    Feat :: use BottomSheet

--- app/src/main/java/com/example/nms_android_v1/feature/main/MainActivity.kt
+++ app/src/main/java/com/example/nms_android_v1/feature/main/MainActivity.kt
@@ -5,10 +5,14 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.nms_android_v1.R
 import com.example.nms_android_v1.base.BaseActivity
 import com.example.nms_android_v1.databinding.ActivityMainBinding
+import com.example.nms_android_v1.feature.main.adapter.BottomSheetAdapter
 import com.example.nms_android_v1.feature.main.adapter.MainAdapter
+import com.example.nms_android_v1.feature.main.fragment.BottomDialogFragment
+import com.example.nms_android_v1.feature.main.model.BottomDialog.BottomDialogData
 import com.example.nms_android_v1.feature.main.model.Notices
 import com.example.nms_android_v1.feature.main.model.PostsResponse
 import com.example.nms_android_v1.feature.main.model.Writer
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : BaseActivity<ActivityMainBinding>(
@@ -22,11 +26,32 @@ class MainActivity : BaseActivity<ActivityMainBinding>(
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val bottomSheetView = layoutInflater.inflate(R.layout.bottom_sheet_dialog, null)
+        val bottomSheetDialog = BottomSheetDialog(this)
+        bottomSheetDialog.setContentView(bottomSheetView)
+
+        val listType = mutableListOf(
+            BottomDialogData("교외"),
+            BottomDialogData("교내")
+        )
+
+        val listGrade  = mutableListOf(
+            BottomDialogData("1학년"),
+            BottomDialogData("2학년"),
+            BottomDialogData("3학년")
+        )
+
+        val bottomDialogFragment = BottomDialogFragment(listType, listGrade)
+
+        binding.textView166.setOnClickListener {
+            bottomDialogFragment.show(supportFragmentManager, "Tag")
+        }
+
         binding.rvMain.layoutManager = LinearLayoutManager(this)
         binding.rvMain.setHasFixedSize(true)
-        binding.rvMain.adapter = MainAdapter(applicationContext, postList)
+        binding.rvMain.adapter = MainAdapter(applicationContext, postList, vm)
 
-        vm.getPosts()
+//        vm.getPosts()
     }
 
     private fun setPosts(postsResponse: PostsResponse) {
@@ -43,9 +68,11 @@ class MainActivity : BaseActivity<ActivityMainBinding>(
             })
 
             failed.observe(this@MainActivity, {
-                it.run {
-                    showToast("데이터 통신에 실패하였습니다.")
-                }
+                showToast(it.toString())
+            })
+
+            toastMessage.observe(this@MainActivity, {
+                showToast(it.toString())
             })
         }
     }
