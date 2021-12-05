5a7cbd3fe76de9427b502a84513b90c9c0e24bb0
유현명 <aahspringaa4@gmail.com>
2021-12-05 16:22:20 +0900

    Feat :: experience

--- app/src/main/java/com/example/nms_android_v1/feature/main/MainActivity.kt
+++ app/src/main/java/com/example/nms_android_v1/feature/main/MainActivity.kt
@@ -15,7 +15,6 @@ class MainActivity : BaseActivity<ActivityMainBinding>(
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
     }
 
     override fun observeEvent() {
