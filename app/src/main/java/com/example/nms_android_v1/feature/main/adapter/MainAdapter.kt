592348e4c17b3433eeb93c27a1508c664e29b227
limsaehyun <sh00710000@gmail.com>
2021-12-05 21:03:58 +0900

    Feat :: 메인 Rv Set

--- app/src/main/java/com/example/nms_android_v1/feature/main/adapter/MainAdapter.kt
+++ app/src/main/java/com/example/nms_android_v1/feature/main/adapter/MainAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.nms_android_v1.feature.main.adapter
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,12 +10,15 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.nms_android_v1.R
 import com.example.nms_android_v1.databinding.MainItemBinding
+import com.example.nms_android_v1.feature.main.MainViewModel
 import com.example.nms_android_v1.feature.main.model.Notices
 import com.example.nms_android_v1.feature.main.model.Writer
+import com.example.nms_android_v1.feature.post.PostActivity
 
 class MainAdapter(
     val context: Context,
-    val productData: List<Notices>
+    val productData: List<Notices>,
+    val vm: MainViewModel
 
     ) : RecyclerView.Adapter<MainAdapter.Holder>() {
 
@@ -32,11 +36,36 @@ class MainAdapter(
     }
 
     override fun onBindViewHolder(holder: Holder, position: Int) {
-        var data: Notices = productData.get(position)
 
-        Glide.with(holder.itemView.context)
-            .load(data.writer.profile_url)
-            .into(binding.ivPostProfile)
+        binding.run {
+            productData.get(position).run {
+                Glide.with(holder.itemView.context)
+                    .load(writer.profile_url)
+                    .into(ivPostProfile)
+
+                tvPostMemberName.text = writer.name
+                tvPostDate.text = updated_date
+                tvPostTitle.text = title
+                tvPostContent.text = content
+                tvPostLiked.text = star_count.toString()
+                tvPostCommtents.text = "댓글 ${comment_count}"
+
+                clItem.setOnClickListener {
+                    val intent = Intent(holder.itemView.context, PostActivity::class.java)
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                    intent.putExtra("noticeId", notice_id)
+                    holder.itemView.context.startActivity(intent)
+                }
+
+                lnLike.setOnClickListener {
+                    if(is_star) {
+                        vm.unstar(notice_id.toString())
+                    } else {
+                        vm.star(notice_id.toString())
+                    }
+                }
+            }
+        }
 
     }
 
