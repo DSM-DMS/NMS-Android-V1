37d2d27e13b6311ef32d12ddbe92bc7048de088d
유현명 <aahspringaa4@naver.com>
2021-12-06 23:08:58 +0900

    Fix :: 잘못된 Response Class 수정

--- app/src/main/java/com/example/nms_android_v1/data/mypage/MypageRepository.kt
+++ app/src/main/java/com/example/nms_android_v1/data/mypage/MypageRepository.kt
@@ -3,6 +3,7 @@ package com.example.nms_android_v1.data.mypage
 import ACCESS_TOKEN
 import com.example.nms_android_v1.di.mypageApi
 import com.example.nms_android_v1.feature.main.model.PostsResponse
+import com.example.nms_android_v1.feature.mypage.dto.ResponseMyPageDTO
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.annotations.NonNull
 import io.reactivex.rxjava3.core.Single
@@ -10,7 +11,7 @@ import io.reactivex.rxjava3.schedulers.Schedulers
 import retrofit2.Response
 
 class MypageRepository {
-    fun getMyPage() : @NonNull Single<Response<PostsResponse>> =
+    fun getMyPage() : @NonNull Single<Response<ResponseMyPageDTO>> =
         mypageApi.getMypage(ACCESS_TOKEN)
             .observeOn(AndroidSchedulers.mainThread())
             .subscribeOn(Schedulers.io())
