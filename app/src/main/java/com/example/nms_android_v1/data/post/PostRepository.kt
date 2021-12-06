ffb8a8c3d7377bca5dfa6cd8f9978353b75cc38e
limsaehyun <sh00710000@gmail.com>
2021-12-06 23:29:24 +0900

    Fix :: return값 변경

--- app/src/main/java/com/example/nms_android_v1/data/post/PostRepository.kt
+++ app/src/main/java/com/example/nms_android_v1/data/post/PostRepository.kt
@@ -3,6 +3,7 @@ package com.example.nms_android_v1.data.post
 import ACCESS_TOKEN
 import com.example.nms_android_v1.di.postApi
 import com.example.nms_android_v1.feature.main.model.PostsResponse
+import com.example.nms_android_v1.feature.post.dto.ResponsePostDTO
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.annotations.NonNull
 import io.reactivex.rxjava3.core.Single
@@ -10,7 +11,7 @@ import io.reactivex.rxjava3.schedulers.Schedulers
 import retrofit2.Response
 
 class PostRepository {
-    fun getPostDetail() : @NonNull Single<Response<PostsResponse>> =
+    fun getPostDetail() : @NonNull Single<Response<ResponsePostDTO>> =
         // notice id 오류 방지를 위해 예시로 넣어둠
         postApi.getPostDetail(ACCESS_TOKEN, notice_id = 123)
             .observeOn(AndroidSchedulers.mainThread())
