ff5ec283f932da468f6b0424b7d3b05f57a7ba8f
유현명 <aahspringaa4@naver.com>
2021-12-08 15:25:56 +0900

    Feat :: 게시물 자세히 보기 구현

--- app/src/main/java/com/example/nms_android_v1/feature/post/ui/PostActivity.kt
+++ app/src/main/java/com/example/nms_android_v1/feature/post/ui/PostActivity.kt
@@ -1,12 +1,32 @@
  package com.example.nms_android_v1.feature.post.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.nms_android_v1.R
+import com.example.nms_android_v1.base.BaseActivity
+import com.example.nms_android_v1.databinding.ActivityPostBinding
+import com.example.nms_android_v1.feature.mypage.adapter.LikePostAdapter
+import com.example.nms_android_v1.feature.post.model.viewmodel.PostViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
- class PostActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_post)
-    }
-}
+ class PostActivity : BaseActivity<ActivityPostBinding>(
+     R.layout.activity_post
+ ) {
+
+     val vm: PostViewModel by viewModel()
+
+     private lateinit var rv: RecyclerView
+     private lateinit var MypageAdpater: LikePostAdapter
+     private lateinit var gridLayoutManager: GridLayoutManager
+
+     override fun onCreate(savedInstanceState: Bundle?) {
+         super.onCreate(savedInstanceState)
+     }
+
+     override fun observeEvent() {
+         vm.run {
+
+         }
+     }
+ }
