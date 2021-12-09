2b9c411d15502983af63807594667fa465f68c21
유현명 <aahspringaa4@naver.com>
2021-12-09 12:20:34 +0900

    Feat :: 좋아요 표시 및 좋아요 취소 구현

--- app/src/main/java/com/example/nms_android_v1/feature/post/ui/PostActivity.kt
+++ app/src/main/java/com/example/nms_android_v1/feature/post/ui/PostActivity.kt
@@ -1,5 +1,6 @@
  package com.example.nms_android_v1.feature.post.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -8,8 +9,11 @@ import com.example.nms_android_v1.base.BaseActivity
 import com.example.nms_android_v1.databinding.ActivityPostBinding
 import com.example.nms_android_v1.feature.main.model.Notices
 import com.example.nms_android_v1.feature.mypage.adapter.LikePostAdapter
+import com.example.nms_android_v1.feature.post.dto.ResponsePostDTO
+import com.example.nms_android_v1.feature.post.model.PostData
 import com.example.nms_android_v1.feature.post.model.viewmodel.PostViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import kotlin.properties.Delegates
 
  class PostActivity : BaseActivity<ActivityPostBinding>(
      R.layout.activity_post
@@ -22,9 +26,24 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
      private lateinit var rv: RecyclerView
      private lateinit var MypageAdpater: LikePostAdapter
      private lateinit var gridLayoutManager: GridLayoutManager
+     private lateinit var PostData: ResponsePostDTO
+     var notice_id by Delegates.notNull<Int>()
 
      override fun onCreate(savedInstanceState: Bundle?) {
          super.onCreate(savedInstanceState)
+
+         val intent = intent
+         intent.getIntExtra("notice_id", notice_id)
+
+         vm.getPostDetail()
+
+         binding.lnLike.setOnClickListener{
+             if(PostData.is_star) {
+                 vm.unStar(notice_id.toString())
+             } else {
+                 vm.star(notice_id.toString())
+             }
+         }
      }
 
      override fun observeEvent() {
