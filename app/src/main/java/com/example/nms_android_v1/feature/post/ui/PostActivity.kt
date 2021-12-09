1392a5bc772f3c74ad3935f29f6f8a9c31d85bda
유현명 <aahspringaa4@naver.com>
2021-12-09 14:29:04 +0900

    Feat :: 게시물 자세히 보기 구현

--- app/src/main/java/com/example/nms_android_v1/feature/post/ui/PostActivity.kt
+++ app/src/main/java/com/example/nms_android_v1/feature/post/ui/PostActivity.kt
@@ -2,17 +2,24 @@
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.KeyCharacterMap.load
+import android.view.PointerIcon.load
+import androidx.core.view.PointerIconCompat.load
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.nms_android_v1.R
 import com.example.nms_android_v1.base.BaseActivity
 import com.example.nms_android_v1.databinding.ActivityPostBinding
 import com.example.nms_android_v1.feature.main.model.Notices
 import com.example.nms_android_v1.feature.mypage.adapter.LikePostAdapter
 import com.example.nms_android_v1.feature.post.dto.ResponsePostDTO
+import com.example.nms_android_v1.feature.post.dto.writer
 import com.example.nms_android_v1.feature.post.model.PostData
 import com.example.nms_android_v1.feature.post.model.viewmodel.PostViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import java.lang.System.load
+import java.util.ServiceLoader.load
 import kotlin.properties.Delegates
 
  class PostActivity : BaseActivity<ActivityPostBinding>(
@@ -27,6 +34,7 @@ import kotlin.properties.Delegates
      private lateinit var MypageAdpater: LikePostAdapter
      private lateinit var gridLayoutManager: GridLayoutManager
      private lateinit var PostData: ResponsePostDTO
+
      var notice_id by Delegates.notNull<Int>()
 
      override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,6 +54,28 @@ import kotlin.properties.Delegates
          }
      }
 
+     private fun setPostDetail(responsePostDTO: ResponsePostDTO){
+         binding.tvTitle.text = responsePostDTO.title
+         binding.tvContent.text = responsePostDTO.content
+         binding.tvWriter.text = responsePostDTO.writer.name
+         binding.tvTarget.text = responsePostDTO.target.target
+         binding.tvPostDate.text = responsePostDTO.created_date
+         binding.tvCommentCount.text = responsePostDTO.comment_count.toString()
+         binding.tvStarCount.text = responsePostDTO.star_count.toString()
+     }
      override fun observeEvent() {
+         vm.run {
+             postDetailData.observe(this@PostActivity, {
+                 setPostDetail(it)
+             })
+
+             failed.observe(this@PostActivity, {
+                 showToast(it.toString())
+             })
+
+             toastMessage.observe(this@PostActivity, {
+                 showToast(it.toString())
+             })
+         }
      }
  }
