93eabdafed9c918cc3356378da94d46f65eea8e4
유현명 <aahspringaa4@naver.com>
2021-12-09 09:19:56 +0900

    Feat :: 마이페이지 좋아하는 게시물 RecyclerView 구현

--- app/src/main/java/com/example/nms_android_v1/feature/mypage/adapter/LikePostAdapter.kt
+++ app/src/main/java/com/example/nms_android_v1/feature/mypage/adapter/LikePostAdapter.kt
@@ -1,30 +1,66 @@
 package com.example.nms_android_v1.feature.mypage.adapter
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.nms_android_v1.R
+import com.example.nms_android_v1.databinding.ListpostItemBinding
+import com.example.nms_android_v1.databinding.MainItemBinding
+import com.example.nms_android_v1.feature.main.adapter.MainAdapter
+import com.example.nms_android_v1.feature.main.model.Notices
+import com.example.nms_android_v1.feature.main.viewmodel.MainViewModel
+import com.example.nms_android_v1.feature.mypage.dto.liked_notices_value
 import com.example.nms_android_v1.feature.mypage.model.LikePostData
+import com.example.nms_android_v1.feature.post.model.viewmodel.PostViewModel
+import com.example.nms_android_v1.feature.post.ui.PostActivity
 
-class LikePostAdapter(private val context: Context?) :
-    RecyclerView.Adapter<LikePostAdapter.ViewHolder>() {
+class LikePostAdapter(
+    private val context: Context,
+    private val LikePostData: List<liked_notices_value>,
+    private val vm: PostViewModel  
+) : RecyclerView.Adapter<LikePostAdapter.ViewHolder>() {
 
-    var datas = mutableListOf<LikePostData>()
+    private var datas = mutableListOf<LikePostData>()
+
+    private lateinit var binding: ListpostItemBinding
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(context).inflate(R.layout.listpost_item,parent,false)
-        return ViewHolder(view)
+        binding = DataBindingUtil.inflate<ListpostItemBinding>(LayoutInflater.from(parent.context),
+            R.layout.listpost_item, parent, false)
+
+        return ViewHolder(binding.root)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        binding.run {
+            LikePostData.get(position).run {
+                Glide.with(holder.itemView.context)
+                    .load(image)
+                    .into(likephoto)
 
+                tvTitle.text = title
+                tvWriter.text = writer
+                tvCreatedDate.text = created_date
+
+                totalItem.setOnClickListener {
+                    val intent = Intent(holder.itemView.context, PostActivity::class.java)
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                    intent.putExtra("noticeId", id)
+                    holder.itemView.context.startActivity(intent)
+                }
+            }
+        }
     }
 
-    override fun getItemCount(): Int = datas.size
-
-    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-
+    override fun getItemCount(): Int {
+        return LikePostData.size
     }
 }
