f56807ea33549d771b3b936d6b4071149ed66021
유현명 <aahspringaa4@naver.com>
2021-12-10 13:54:45 +0900

    Feat :: class 이름 수정

--- app/src/main/java/com/example/nms_android_v1/feature/mypage/dto/ResponseMyPageDTO.kt
+++ app/src/main/java/com/example/nms_android_v1/feature/mypage/dto/ResponseMyPageDTO.kt
@@ -8,5 +8,5 @@ data class ResponseMyPageDTO(
     @SerializedName("gcn") val gcn: String,
     @SerializedName("email") val email: String,
     @SerializedName("profile_url") val profile_url: String,
-    @SerializedName("liked_notices_value") val likedNoticesValue: liked_notices_value
+    @SerializedName("liked_notices_value") val likedNoticesValue: Liked_Notices_Value
 )
