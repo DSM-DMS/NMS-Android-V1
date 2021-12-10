596758ec81b258cf467910f8c05a018d35f1e77b
유현명 <aahspringaa4@naver.com>
2021-12-10 14:48:45 +0900

    Feat :: Activity ViewModel 연동

--- app/src/main/java/com/example/nms_android_v1/feature/mypage/ui/MyInformationActivity.kt
+++ app/src/main/java/com/example/nms_android_v1/feature/mypage/ui/MyInformationActivity.kt
@@ -2,10 +2,18 @@ package com.example.nms_android_v1.feature.mypage.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.nms_android_v1.R
+import com.example.nms_android_v1.base.BaseActivity
+import com.example.nms_android_v1.databinding.ActivityMyInformationBinding
+import com.example.nms_android_v1.databinding.ActivityProfileBinding
 
-class MyInformationActivity : AppCompatActivity() {
+class MyInformationActivity : BaseActivity<ActivityMyInformationBinding>(
+    R.layout.activity_my_information
+) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_my_information)
+    }
+
+    override fun observeEvent() {
     }
 }
