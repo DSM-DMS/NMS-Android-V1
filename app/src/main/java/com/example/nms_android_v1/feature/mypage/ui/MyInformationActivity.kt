171a098b2f3b3b32c82fd7cee5073ce94cdf6f78
유현명 <aahspringaa4@naver.com>
2021-12-10 14:48:15 +0900

    Feat :: 나의 정보 확인 페이지 Activity & XML 생성

---
+++ app/src/main/java/com/example/nms_android_v1/feature/mypage/ui/MyInformationActivity.kt
@@ -0,0 +1,11 @@
+package com.example.nms_android_v1.feature.mypage.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyInformationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_information)
+    }
+}
