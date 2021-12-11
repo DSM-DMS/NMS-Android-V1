7042342cad07d781aef08201754068482fc47658
saehyun <sh00710000@gmail.com>
2021-12-11 11:20:48 +0900

    Feat :: 이메일 인증 관련 Repository

--- app/src/main/java/com/example/nms_android_v1/data/register/RegisterRepository.kt
+++ app/src/main/java/com/example/nms_android_v1/data/register/RegisterRepository.kt
@@ -1,9 +1,8 @@
 package com.example.nms_android_v1.data.register
 
-import com.example.nms_android_v1.di.loginApi
 import com.example.nms_android_v1.di.registerApi
-import com.example.nms_android_v1.feature.login.model.LoginRequest
-import com.example.nms_android_v1.feature.login.model.LoginResponse
+import com.example.nms_android_v1.feature.login.model.CheckEmailCertifiedRequest
+import com.example.nms_android_v1.feature.login.model.SendEmailCertifiedRequest
 import com.example.nms_android_v1.feature.register.model.RegisterRequest
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.annotations.NonNull
@@ -18,4 +17,13 @@ class RegisterRepository {
             .observeOn(AndroidSchedulers.mainThread())
             .subscribeOn(Schedulers.io())
 
+    fun sendEmailCertified(email: String) : @NonNull Single<Response<Void>> =
+        registerApi.sendEmailCertified(SendEmailCertifiedRequest(email))
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.io())
+
+    fun checkEmailCertified(email: String, code: String) : @NonNull Single<Response<Void>> =
+        registerApi.checkEmailCertified(CheckEmailCertifiedRequest(email, code))
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.io())
 }
