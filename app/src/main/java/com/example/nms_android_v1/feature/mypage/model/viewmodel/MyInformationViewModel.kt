3de6f41a2cdd0244932621c5c44baf4b9569b7a3
유현명 <aahspringaa4@naver.com>
2021-12-11 14:40:12 +0900

    Feat :: 내 정보 수정 ViewModel 구현

--- app/src/main/java/com/example/nms_android_v1/feature/mypage/model/viewmodel/MyInformationViewModel.kt
+++ app/src/main/java/com/example/nms_android_v1/feature/mypage/model/viewmodel/MyInformationViewModel.kt
@@ -1,4 +1,25 @@
 package com.example.nms_android_v1.feature.mypage.model.viewmodel
 
-class MyInformationViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.nms_android_v1.data.mypage.MypageRepository
+import com.example.nms_android_v1.feature.mypage.dto.ResponseMyPageDTO
+
+class MyInformationViewModel(
+    private val repository: MypageRepository,
+) : ViewModel() {
+
+    val toastMessage : MutableLiveData<String> = MutableLiveData()
+    val failed : MutableLiveData<Boolean> = MutableLiveData()
+    val MypageData : MutableLiveData<ResponseMyPageDTO> = MutableLiveData()
+
+    fun myInformation() {
+        repository.getInformation().subscribe { response ->
+            if(response.isSuccessful) {
+                MypageData.value = response.body()
+            } else {
+                failed.value = true
+            }
+        }
+    }
 }
