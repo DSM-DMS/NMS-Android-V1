b2ab56da0a9431cbaad8c892ad2282189fcacfb0
saehyun <sh00710000@gmail.com>
2021-12-11 13:52:24 +0900

    Feat :: RxJava3 Error Handler

--- app/src/main/java/com/example/nms_android_v1/feature/main/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/nms_android_v1/feature/main/ui/activity/MainActivity.kt
@@ -1,18 +1,23 @@
 package com.example.nms_android_v1.feature.main.ui.activity
 
 import android.os.Bundle
+import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.nms_android_v1.R
 import com.example.nms_android_v1.base.BaseActivity
 import com.example.nms_android_v1.databinding.ActivityMainBinding
 import com.example.nms_android_v1.feature.main.adapter.MainAdapter
-import com.example.nms_android_v1.feature.main.ui.fragment.BottomDialogFragment
 import com.example.nms_android_v1.feature.main.model.BottomDialog.BottomDialogData
-import com.example.nms_android_v1.feature.main.model.Notices
+import com.example.nms_android_v1.feature.main.model.Notice
 import com.example.nms_android_v1.feature.main.model.PostsResponse
+import com.example.nms_android_v1.feature.main.ui.fragment.BottomDialogFragment
 import com.example.nms_android_v1.feature.main.viewmodel.MainViewModel
 import com.google.android.material.bottomsheet.BottomSheetDialog
+import io.reactivex.rxjava3.exceptions.UndeliverableException
+import io.reactivex.rxjava3.plugins.RxJavaPlugins
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import java.io.IOException
+import java.net.SocketException
 
 class MainActivity : BaseActivity<ActivityMainBinding>(
     R.layout.activity_main
@@ -20,10 +25,36 @@ class MainActivity : BaseActivity<ActivityMainBinding>(
 
     val vm: MainViewModel by viewModel()
 
-    val postList = arrayListOf<Notices>()
+    val postList = arrayListOf<Notice>()
+
+    private lateinit var mainAdapter: MainAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        RxJavaPlugins.setErrorHandler { e ->
+            var error = e
+            if (error is UndeliverableException) {
+                error = e.cause
+            }
+            if (error is IOException || error is SocketException) {
+                return@setErrorHandler
+            }
+            if (error is InterruptedException) {
+                return@setErrorHandler
+            }
+            if (error is NullPointerException || error is IllegalArgumentException) {
+                Thread.currentThread().uncaughtExceptionHandler
+                    .uncaughtException(Thread.currentThread(), error)
+                return@setErrorHandler
+            }
+            if (error is IllegalStateException) {
+                Thread.currentThread().uncaughtExceptionHandler
+                    .uncaughtException(Thread.currentThread(), error)
+                return@setErrorHandler
+            }
+            Log.w("Undeliverable exception received, not sure what to do", error)
+        }
 
         val bottomSheetView = layoutInflater.inflate(R.layout.bottom_sheet_dialog, null)
         val bottomSheetDialog = BottomSheetDialog(this)
@@ -49,21 +80,24 @@ class MainActivity : BaseActivity<ActivityMainBinding>(
             bottomDialogFragment.show(supportFragmentManager, "Tag")
         }
 
+        mainAdapter = MainAdapter(applicationContext, postList, vm)
+
         binding.rvMain.layoutManager = LinearLayoutManager(this)
         binding.rvMain.setHasFixedSize(true)
-        binding.rvMain.adapter = MainAdapter(applicationContext, postList, vm)
+        binding.rvMain.adapter = mainAdapter
 
         vm.getPosts()
     }
 
     private fun setPosts(postsResponse: PostsResponse) {
-        for(i: Int in 1..postsResponse.notice_count) {
+        for(i: Int in 0..postsResponse.notice_count-1) {
             postList.add(postsResponse.notices.get(i))
         }
         binding.rvMain.adapter?.notifyDataSetChanged()
     }
 
     override fun observeEvent() {
+
         vm.run {
             postsData.observe(this@MainActivity, {
                 setPosts(it)
@@ -75,6 +109,13 @@ class MainActivity : BaseActivity<ActivityMainBinding>(
 
             toastMessage.observe(this@MainActivity, {
                 showToast(it.toString())
+
+                if(it == "좋아요 성공") {
+                    mainAdapter.setLikeOn()
+                }
+                if(it == "취소 성공") {
+                    mainAdapter.setLikeOff()
+                }
             })
         }
     }
