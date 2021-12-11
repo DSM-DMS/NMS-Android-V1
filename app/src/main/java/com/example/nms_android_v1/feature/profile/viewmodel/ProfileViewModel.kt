b0f6bf7a1f520cee7e76cd238899a5ab59f282c3
유현명 <aahspringaa4@naver.com>
2021-12-11 14:37:39 +0900

    Feat :: Profile ViewModel 기초 구현

--- app/src/main/java/com/example/nms_android_v1/feature/profile/viewmodel/ProfileViewModel.kt
+++ app/src/main/java/com/example/nms_android_v1/feature/profile/viewmodel/ProfileViewModel.kt
@@ -1,4 +1,9 @@
 package com.example.nms_android_v1.feature.profile.viewmodel
 
-class ProfileViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.nms_android_v1.data.profile.ProfileRepository
+
+class ProfileViewModel(
+    private val repository: ProfileRepository,
+) : ViewModel() {
 }
