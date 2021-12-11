afcedac9a722784063fcb9ff4af560eb303d9f65
saehyun <sh00710000@gmail.com>
2021-12-11 13:52:45 +0900

    Fix :: 오류 시 대응

--- app/src/main/java/com/example/nms_android_v1/feature/main/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/nms_android_v1/feature/main/viewmodel/MainViewModel.kt
@@ -1,12 +1,12 @@
 package com.example.nms_android_v1.feature.main.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.nms_android_v1.data.main.MainRepository
 import com.example.nms_android_v1.data.star.StarRepository
-import com.example.nms_android_v1.feature.main.model.Notices
+import com.example.nms_android_v1.feature.main.model.Notice
 import com.example.nms_android_v1.feature.main.model.PostsResponse
-import com.example.nms_android_v1.feature.main.model.Targets
 
 class MainViewModel(
     private val mp: MainRepository,
@@ -18,13 +18,15 @@ class MainViewModel(
     val postsData : MutableLiveData<PostsResponse> = MutableLiveData()
 
     fun getPosts() {
-        mp.getPosts().subscribe { response ->
+        mp.getPosts().subscribe( { response ->
             if(response.isSuccessful) {
-                postsData.value = response.body()
+                postsData.value = response.body()!!
             } else {
                 failed.value = true
             }
-        }
+        }, {
+            Log.d("Main", "getPosts: ${it.message}")
+        })
     }
 
     fun getTargetPosts(target: String) {
@@ -42,17 +44,23 @@ class MainViewModel(
             if(response.isSuccessful) {
                 toastMessage.value = "좋아요 성공"
             } else {
-                toastMessage.value = "데이터 통신 실패"
+                when(response.code()) {
+                    409 -> unstar(noticeId)
+                    else -> toastMessage.value = "데이터 통신 실패"
+                }
             }
         }
     }
 
     fun unstar(noticeId: String) {
-        sp.star(noticeId).subscribe { response ->
+        sp.unstar(noticeId).subscribe { response ->
             if(response.isSuccessful) {
                 toastMessage.value = "취소 성공"
             } else {
-                toastMessage.value = "데이터 통신 실패"
+                when(response.code()) {
+                    409 -> star(noticeId)
+                    else -> toastMessage.value = "데이터 통신 실패"
+                }
             }
         }
     }
