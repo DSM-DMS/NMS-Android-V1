3e56fa41cdb6be1eb1f80fc14cc592af31d10049
saehyun <sh00710000@gmail.com>
2021-12-11 11:21:01 +0900

    Feat :: CheckEmailCertifiedRequest

---
+++ app/src/main/java/com/example/nms_android_v1/feature/login/model/CheckEmailCertifiedRequest.kt
@@ -0,0 +1,9 @@
+package com.example.nms_android_v1.feature.login.model
+
+import com.google.gson.annotations.SerializedName
+import retrofit2.http.Body
+
+data class CheckEmailCertifiedRequest(
+    @SerializedName("email") val email: String,
+    @SerializedName("code") val code: String,
+)
