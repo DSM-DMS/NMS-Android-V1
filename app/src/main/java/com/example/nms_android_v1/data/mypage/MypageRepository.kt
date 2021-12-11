d063718df2a2b3c44a6119a81a57ba18aae29f40
유현명 <aahspringaa4@naver.com>
2021-12-11 14:39:14 +0900

    Feat :: 내 정보 수정 화면 옵저브 추가

--- app/src/main/java/com/example/nms_android_v1/data/mypage/MypageRepository.kt
+++ app/src/main/java/com/example/nms_android_v1/data/mypage/MypageRepository.kt
@@ -3,6 +3,7 @@ package com.example.nms_android_v1.data.mypage
 import ACCESS_TOKEN
 import com.example.nms_android_v1.di.mypageApi
 import com.example.nms_android_v1.feature.main.model.PostsResponse
+import com.example.nms_android_v1.feature.mypage.dto.ResponseMyInformationDTO
 import com.example.nms_android_v1.feature.mypage.dto.ResponseMyPageDTO
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.annotations.NonNull
@@ -12,6 +13,11 @@ import retrofit2.Response
 
 class MypageRepository {
     fun getMyPage() : @NonNull Single<Response<ResponseMyPageDTO>> =
+        mypageApi.getMypage(ACCESS_TOKEN)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.io())
+
+    fun getInformation() : @NonNull Single<Response<ResponseMyPageDTO>> =
         mypageApi.getMypage(ACCESS_TOKEN)
             .observeOn(AndroidSchedulers.mainThread())
             .subscribeOn(Schedulers.io())
