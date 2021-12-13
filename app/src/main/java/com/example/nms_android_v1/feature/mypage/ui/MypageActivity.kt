915288343bc2903f2d5d3bff7b3e599111bbad2e
유현명 <aahspringaa4@naver.com>
2021-12-13 23:14:12 +0900

    Fix :: 잔 오류 수정

--- app/src/main/java/com/example/nms_android_v1/feature/mypage/ui/MypageActivity.kt
+++ app/src/main/java/com/example/nms_android_v1/feature/mypage/ui/MypageActivity.kt
@@ -1,16 +1,20 @@
 package com.example.nms_android_v1.feature.mypage.ui
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import com.example.nms_android_v1.R
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.nms_android_v1.base.BaseActivity
 import com.example.nms_android_v1.databinding.ActivityMypageBinding
 import com.example.nms_android_v1.feature.mypage.adapter.LikePostAdapter
+import com.example.nms_android_v1.feature.mypage.dto.Liked_Notices_Value
 import com.example.nms_android_v1.feature.mypage.dto.ResponseMyPageDTO
 import com.example.nms_android_v1.feature.mypage.model.viewmodel.MypageViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import java.lang.System.load
 
 
 class MypageActivity : BaseActivity<ActivityMypageBinding>(
@@ -19,13 +23,17 @@ class MypageActivity : BaseActivity<ActivityMypageBinding>(
 
     private val vm: MypageViewModel by viewModel()
 
+    private var data = arrayListOf<Liked_Notices_Value>()
+
     private lateinit var MypageAdpater: LikePostAdapter
     private lateinit var gridLayoutManager: GridLayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val gridLayoutManager = GridLayoutManager(this, 2, GridLayoutManager.HORIZONTAL, false)
+
+        val gridLayoutManager = GridLayoutManager(this, 2, GridLayoutManager.VERTICAL, false)
         binding.rvMyPage.layoutManager = gridLayoutManager
+        binding.rvMyPage.adapter = LikePostAdapter(applicationContext, data)
 
         vm.myPage()
 
@@ -38,16 +46,25 @@ class MypageActivity : BaseActivity<ActivityMypageBinding>(
     private fun setMypage(ResponseMyPage: ResponseMyPageDTO) {
         binding.tvName.text = ResponseMyPage.name
         binding.tvEmail.text = ResponseMyPage.email
+
+        Glide.with(applicationContext)
+            .load(ResponseMyPage.profile_url)
+            .into(binding.ivProfile)
     }
 
+    private fun setData(likeData: List<Liked_Notices_Value>) {
+        for(i: Int in 0..likeData.size) {
+
+            data.add(likeData.get(i))
+
+            binding.rvMyPage.adapter?.notifyDataSetChanged()
+        }
+    }
     override fun observeEvent() {
         vm.run {
             MypageData.observe(this@MypageActivity, {
                 setMypage(it)
-            })
-
-            failed.observe(this@MypageActivity, {
-                showToast(it.toString())
+//                setData(it.started_notice)
             })
 
             toastMessage.observe(this@MypageActivity, {
